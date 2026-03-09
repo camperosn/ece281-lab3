4e89a9c189bc50e2ec19cc35f94c13d23eefa24e
Camperos <178922401+camperosn@users.noreply.github.com>
2026-03-09 10:23:13 -0600

    day 1 of lab - paused on test bench implemnentation

--- src/thunderbird_fsm_tb.vhd
+++ src/thunderbird_fsm_tb.vhd
@@ -57,28 +57,114 @@ end thunderbird_fsm_tb;
 architecture test_bench of thunderbird_fsm_tb is 
 	
 	component thunderbird_fsm is 
---	  port(
-		
---	  );
+    port(
+        i_clk, i_reset  : in    std_logic;
+        i_left, i_right : in    std_logic;
+        o_lights_L      : out   std_logic_vector(2 downto 0);
+        o_lights_R      : out   std_logic_vector(2 downto 0)
+    );
 	end component thunderbird_fsm;
 
 	-- test I/O signals
 	
+	-- Inputs
+	signal w_i_left : std_logic := '0';
+	signal w_i_right : std_logic := '0';
+	signal w_reset : std_logic := '0';
+	signal w_clk : std_logic := '0';
+	
+	-- Outputs
+	signal w_o_lights_L : std_logic_vector(2 downto 0) := "000";
+	signal w_o_lights_R : std_logic_vector(2 downto 0) := "000";
+
 	-- constants
+	constant k_clk_period : time := 10ns;
 	
 	
 begin
 	-- PORT MAPS ----------------------------------------
-	
+	uut: thunderbird_fsm port map (
+	   i_left => w_i_left,
+	   i_right => w_i_right,
+	   i_reset => w_reset,
+	   i_clk => w_clk,
+	   o_lights_L => w_o_lights_L,
+	   o_lights_R => w_o_lights_R
+	);
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
     -- Clock process ------------------------------------
-    
+    clk_proc : process
+    begin
+        w_clk <= '0';
+        wait for k_clk_period/2;
+        w_clk <= '1';
+        wait for k_clk_period/2;
+    end process;
 	-----------------------------------------------------
 	
 	-- Test Plan Process --------------------------------
+	sim_proc : process
+	begin
 	
+	   w_reset <= '1';
+	   wait for k_clk_period*1;
+	       assert w_o_lights_L = "000" report "bad reset" severity failure;
+	       assert w_o_lights_R = "000" report "bad reset" severity failure;
+	   
+	   w_reset <= '0';
+	   wait for k_clk_period*1;
+	   
+	   -- Test 1: Activate left - disable midway
+	   
+	   -- Enable - should begin cycle
+	   w_i_left <= '1'; wait for k_clk_period;
+	       assert w_o_lights_L = "100" report "bad left cycle 1" severity failure;
+	   wait for k_clk_period;
+	       assert w_o_lights_L = "110" report "bad left cycle 2" severity failure;
+	   wait for k_clk_period;
+	       assert w_o_lights_L = "111" report "bad left cycle 3" severity failure;
+	   wait for k_clk_period;
+	       assert w_o_lights_L = "000" report "bad left cycle 4" severity failure;
+	   wait for k_clk_period;
+	       assert w_o_lights_L = "100" report "bad left cycle 5" severity failure;
+	   -- Disable - should complete cycle then stop
+	   w_i_left <= '0'; wait for k_clk_period;
+	  	   assert w_o_lights_L = "110" report "bad left cycle 6" severity failure;
+	   wait for k_clk_period;
+	       assert w_o_lights_L = "111" report "bad left cycle 7" severity failure;
+	   wait for k_clk_period*2; -- wait 2 cycles
+	       assert w_o_lights_L = "000" report "bad left cycle 8" severity failure;
+	   
+	           	  	   
+	   -- Test 2: Activate right, disable midway
+        -- Enable - should begin cycle
+	   w_i_right <= '1'; wait for k_clk_period;
+	       assert w_o_lights_R = "000" report "bad right cycle 1" severity failure;
+	   wait for k_clk_period;
+	       assert w_o_lights_R = "011" report "bad right cycle 2" severity failure;
+	   wait for k_clk_period;
+	       assert w_o_lights_R = "111" report "bad right cycle 3" severity failure;
+	   wait for k_clk_period;
+	       assert w_o_lights_R = "000" report "bad right cycle 4" severity failure;
+	   wait for k_clk_period;
+	       assert w_o_lights_R = "001" report "bad right cycle 5" severity failure;
+	   -- Disable - should complete cycle then stop
+	   w_i_right <= '0'; wait for k_clk_period;
+	  	   assert w_o_lights_R = "011" report "bad right cycle 6" severity failure;
+	   wait for k_clk_period;
+	       assert w_o_lights_R = "111" report "bad right cycle 7" severity failure;
+	   wait for k_clk_period*2; -- wait 2 cycles
+	       assert w_o_lights_R = "000" report "bad right cycle 8" severity failure;	   
+
+
+	   
+	   -- Test 5: Active both
+	   	
+	-- Tests complete
+	   	wait;
+	end process;
 	-----------------------------------------------------	
 	
 end test_bench;
