5741194226f1cb960a4de3b060adda9ee14bd368
Camperos <178922401+camperosn@users.noreply.github.com>
2026-03-11 09:45:32 -0600

    fixed spacing error

--- src/thunderbird_fsm_tb.vhd
+++ src/thunderbird_fsm_tb.vhd
@@ -80,7 +80,7 @@ architecture test_bench of thunderbird_fsm_tb is
 	signal w_o_lights_R : std_logic_vector(2 downto 0) := "000";
 
 	-- constants
-	constant k_clk_period : time := 10ns;
+	constant k_clk_period : time := 10 ns;
 	
 	
 begin
